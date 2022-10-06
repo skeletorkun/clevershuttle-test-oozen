1857c8273c7f3381eb86441a139b5abd70d4a964
Orkun Ozen <oozen@vulog.com>
2022-10-06 18:04:04 +0300

    Project structure only.
    Context loads ok.

---
+++ clevershuttle/src/test/java/com/example/clevershuttle/CleverShuttleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clevershuttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverShuttleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
