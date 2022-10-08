600588e95da56d59dbfdf45a93db2f3733d0e76f
Orkun Ozen <oozen@vulog.com>
2022-10-08 13:44:03 +0300

    Added validation and improved tests

--- clevershuttle/src/main/java/com/example/clevershuttle/web/model/CarDto.java
+++ clevershuttle/src/main/java/com/example/clevershuttle/web/model/CarDto.java
@@ -7,9 +7,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
 import java.time.OffsetDateTime;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 
 @Data
 @NoArgsConstructor
@@ -19,16 +21,29 @@ public class CarDto {
 
     static final long serialVersionUID = -5815566940065181210L;
 
+    @Null
     private Long id;
+
+    @NotNull
     private String brand;
+
+    @NotBlank
     private String licensePlate;
+
+    @NotNull
     private String manufacturer;
+
+    @NotNull
     private Long operationCity;
+
+    @NotNull
     private Status status;
 
+    @Null
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdAt;
 
+    @Null
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastUpdatedAt;
 }
--- clevershuttle/src/test/java/com/example/clevershuttle/web/controller/CarControllerTest.java
+++ clevershuttle/src/test/java/com/example/clevershuttle/web/controller/CarControllerTest.java
@@ -1,15 +1,23 @@
 package com.example.clevershuttle.web.controller;
 
 import com.example.clevershuttle.web.model.CarDto;
+import com.example.clevershuttle.web.model.Status;
 import com.example.clevershuttle.web.services.CarService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CarController.class)
@@ -21,12 +29,23 @@ class CarControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    CarService carService;
+
     @Test
     void getCarById() throws Exception {
 
-        mockMvc.perform(get("/api/car/" + 1).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        given(carService.getById(any())).willReturn(getValidCarDto());
 
+        mockMvc.perform(get("/api/car/" + 1).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.brand", is("Carrera")))
+                .andExpect(jsonPath("$.licensePlate", is("ABC-983-XYZ")))
+                .andExpect(jsonPath("$.manufacturer", is("Porche")))
+                .andExpect(jsonPath("$.operationCity", is(2)))
+                .andExpect(jsonPath("$.status", is("IN_MAINTENANCE")))
+        ;
     }
 
     @Test
@@ -35,9 +54,23 @@ class CarControllerTest {
         CarDto carDto = CarDto.builder().build();
         String carDtoJson = objectMapper.writeValueAsString(carDto);
 
+        given(carService.saveNewCar(any())).willReturn(getValidCarDto());
+
         mockMvc.perform(post("/api/car/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(carDtoJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+        ;
+    }
+
+    CarDto getValidCarDto() {
+        return CarDto.builder()
+                .id(5L)
+                .brand("Carrera")
+                .manufacturer("Porche")
+                .licensePlate("ABC-983-XYZ")
+                .operationCity(2L)
+                .status(Status.IN_MAINTENANCE)
+                .build();
     }
 }
